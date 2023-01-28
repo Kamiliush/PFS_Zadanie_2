e32e8382a31ff1f5006136172c9f28da31ebe1d0
Kamiliush <kamil1574@gmail.com>
2023-01-28 21:13:49 +0100

    Fix

--- client/src/App.js
+++ client/src/App.js
@@ -10,17 +10,8 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React 3
-          </a>
           <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <Link to="/otherpage">Dane studenta</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
